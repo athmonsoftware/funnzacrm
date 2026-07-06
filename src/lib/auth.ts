a04043dc27b9e588fdbab57f4274bdf77d824f80
JoelEkeng <joelekeng23@gmail.com>
2026-07-06 14:55:48 +0000

    Added trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,4 +5,14 @@ export const auth = betterAuth({
 	database: new Pool({
 		connectionString: process.env.DATABASE_URL,
 	}),
+	session: {
+		expiresIn: 60 * 60 * 24 * 2,
+		updateAge: 60 * 60 * 6,
+	},
+	trustedOrigins: [
+		"https://funnzacrm.vercel.app",
+	],
+	advanced: {
+        useSecureCookies: true
+    }
 });
