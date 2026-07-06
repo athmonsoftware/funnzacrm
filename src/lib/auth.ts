8370480c0d4dfadb077513a1da3de54274bb8e58
JoelEkeng <joelekeng23@gmail.com>
2026-07-06 15:55:13 +0000

    Testing

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,14 +5,7 @@ export const auth = betterAuth({
 	database: new Pool({
 		connectionString: process.env.DATABASE_URL,
 	}),
-	session: {
-		expiresIn: 60 * 60 * 24 * 2,
-		updateAge: 60 * 60 * 6,
+	account: {
+		skipStateCookieCheck: true,
 	},
-	trustedOrigins: [
-		"https://funnzacrm.vercel.app",
-	],
-	advanced: {
-        useSecureCookies: true
-    }
 });
